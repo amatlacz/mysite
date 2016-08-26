c757d859a655784172bc481b117b1869adec9aa2
Adam Matlacz <amatlacz@gmail.com>
2016-08-26 18:22:56 +0200

    Viev and templates for blog post details page

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -121,10 +121,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
-#print("STATIC_ROOT path " + STATIC_ROOT)
